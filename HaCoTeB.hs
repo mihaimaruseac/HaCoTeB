bf3394c9be88fe07fa9849cdefe0cb72a1a6500e
Mihai Maruseac <mihai.maruseac@gmail.com>
2010-07-13 09:15:12 +0300

    Alternate try

--- HaCoTeB.hs
+++ HaCoTeB.hs
@@ -1,3 +1,12 @@
 module HaCoTeB where
 
+-- Section datatype
+data Section 
+  = NoHead String
+  | Complete String String
+  deriving Show
+
+-- Section parser
+getSections :: String -> [Section]
+getSections = undefined
 
