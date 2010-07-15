da42c69ed059adbfca5150c886596f4f78259661
Mihai Maruseac <mihai.maruseac@gmail.com>
2010-07-15 09:10:40 +0300

    Still stub

--- HaCoTeB.hs
+++ HaCoTeB.hs
@@ -1,12 +1,15 @@
 module HaCoTeB where
 
+import Debug.Trace
+
 -- Section datatype
-data Section 
-  = NoHead String
-  | Complete String String
-  deriving Show
+data Section = Complete String String deriving Show
 
 -- Section parser
 getSections :: String -> [Section]
-getSections = undefined
+getSections text = trace (show a) undefined
+  where
+    a = lines text
+
+test = "terminator\n[header1]\ntext1\nterminator\n[header2]\ntext2"
 
