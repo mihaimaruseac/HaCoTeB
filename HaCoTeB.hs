57a50512ccc76c4848eb0a3aaf7bfeb7c19ef1e8
Mihai Maruseac <mihai.maruseac@google.com>
2010-07-16 12:51:17 +0300

    Parsing of sections of text (without Happy)

--- HaCoTeB.hs
+++ HaCoTeB.hs
@@ -3,13 +3,41 @@ module HaCoTeB where
 import Debug.Trace
 
 -- Section datatype
-data Section = Complete String String deriving Show
+data Section 
+  = Anon String
+  | Complete String String
+  deriving Show
 
 -- Section parser
-getSections :: String -> [Section]
-getSections text = trace (show a) undefined
-  where
-    a = lines text
 
-test = "terminator\n[header1]\ntext1\nterminator\n[header2]\ntext2"
+{-
+  Parse e text and return a list of its Section. Uses splitSections and
+  createSection.
+-}
+getSections :: String -> [Section]
+getSections text = map createSection $ splitSections (head ls) ls
+  where
+    ls = lines text
+
+{-
+  Create a Section from a list of strings. If first string seems to be a
+  header construct a Complete section, otherwise an Anonymous one.
+-}
+createSection :: [String] -> Section
+createSection text@(header:content)
+  | head header == '[' = Complete header (unlines content)
+  | otherwise = Anon (unlines text)
+
+{-
+  Split the stream of lines from the file into a list of list of lines.
+  The delimiter is the first argument of the function and will not
+  appear on the output.
+-}
+splitSections :: String -> [String] -> [[String]]
+splitSections _ [] = []
+splitSections end (_:content) = section : (splitSections end sections)
+  where
+    (section, sections) = span (/= end) content
+
+test = "terminator\n[header1]\ntext1\nterminator\nanon\n[header2]\ntext2"
 
