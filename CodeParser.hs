803c2a0f42e07db00ab7116c9c6fe79b6bfc93ce
Mihai Maruseac <mihai.maruseac@gmail.com>
2010-07-28 09:01:06 +0300

    Fixed small bug in code generation: the code must keep the lines in the same way

--- CodeParser.hs
+++ CodeParser.hs
@@ -1,15 +1,17 @@
 module CodeParser (representCode) where
 
+import Data.List
 import Text.Html
 
 import DataDefs
+
+import Debug.Trace
 
 {-
   Represents one section of text as an HTML fragment.
 -}
 representCode :: Content -> HeaderName -> HeaderExtra -> Html
 representCode c _ _ = 
-  thecode ! [thestyle "text-align: left-align;"] <<
-  stringToHtml (init.unlines $ c)
-
+    thecode ! [thestyle "text-align: left-align;"] << 
+    linesToHtml c
 
