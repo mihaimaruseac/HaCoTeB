8ac5870505c61600a30f1c7d5c324718df318f98
Mihai Maruseac <mihai.maruseac@rosedu.org>
2011-02-11 08:41:57 +0200

    Added complete definition of HaCoTeB AST

--- src/HaCoTeB/Types.hs
+++ src/HaCoTeB/Types.hs
@@ -10,9 +10,35 @@ newtype AST = AST [Section] deriving (Eq, Show, Read)
 data Section
   = TextSection Content
   | CodeSection Content
+  | DecoratedSection [Decoration] Content
+  deriving (Eq, Show, Read)
+
+data Decoration
+  = Alignment Align
+  | ColorDec String
+  | PreformattedDec
+  | Heading Int
+  deriving (Eq, Show, Read)
+
+data Align
+  = Center
+  | Full
+  | Right
+  | Left
   deriving (Eq, Show, Read)
 
 data Content
   = SimpleContent String
+  | FormattedContent [Format] String
+  | MixedContent [Content]
+  deriving (Eq, Show, Read)
+
+data Format
+  = Bold
+  | Italic
+  | Underline
+  | Strikeout
+  | Preformatted
+  | Color String
   deriving (Eq, Show, Read)
 
