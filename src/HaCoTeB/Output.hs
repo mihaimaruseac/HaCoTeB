cbf45c41774f37e0ecdf14dbad4755122da26f1d
Mihai Maruseac <mihai.maruseac@gmail.com>
2014-02-09 21:51:49 -0500

    Update Output.hs

--- src/HaCoTeB/Output.hs
+++ src/HaCoTeB/Output.hs
@@ -16,8 +16,6 @@ import HaCoTeB.Output.HTMLOutput
 output :: AST -> String
 output = selectOutput
 
-{- TODO: implement a switching mechanism based on options -}
 selectOutput :: AST -> String
 selectOutput = htmlOutput
---selectOutput = textOutput
 
